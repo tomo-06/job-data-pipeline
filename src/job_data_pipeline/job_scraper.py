2ed58a28b1da3ac5dec09cb6ac91276ca71785e5
tomo-06 <0622ryot@gmail.com>
2025-10-04 16:34:35 +0900

    feat: init_driver() と search_jobs() 関数を実装
    ・Chromeドライバの初期化処理を追加
    ・検索ワードを入力して検索実行する処理を実装

---
+++ src/job_data_pipeline/job_scraper.py
@@ -0,0 +1,54 @@
+"""
+リクナビNEXTから求人情報をスクレイピングする
+"""
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# ================================
+# 定数設定
+# ================================
+sleep_time = 3
+csv_name = "../data/rikunabi.csv"
+url = "https://next.rikunabi.com/"
+
+
+# ================================
+# Chrome初期化
+# ================================
+def init_driver() -> webdriver.Chrome:
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--lang=ja")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
+
+
+# ================================
+# ページ遷移処理
+# ================================
+"""検索ワードを入力して検索実行"""
+
+
+def search_jobs(driver: webdriver.Chrome, search_word: str) -> None:
+    driver.get(url)
+    time.sleep(sleep_time)
+
+    input_element = driver.find_element(
+        By.CLASS_NAME, "styles_selectedOptions__3YT3_"
+    ).find_element(By.TAG_NAME, "input")
+    input_element.send_keys(search_word)
+    button_element = driver.find_element(
+        By.CSS_SELECTOR,
+        "styles_button__slVGb.styles_middle__GPwzV.styles_primary__iQFwH.styles_searchButton__Bde1_",
+    )
+    button_element.click()
+    time.sleep(sleep_time)
