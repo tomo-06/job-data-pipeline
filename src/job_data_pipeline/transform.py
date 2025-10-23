ae868f56d00b67bc08932f46aa9f9085328303fa
tomo-06 <0622ryot@gmail.com>
2025-10-23 22:39:57 +0900

    feat: clean_company_name(name)関数の実装
    ・企業名のクレンジングを行う関数を実装
    企業名のクレンジング処理
    全角スペースを半角に変換
    本店・支店・東京・大阪などのノイズを削除
    省略表記の正規化（(株) → 株式会社）

--- src/job_data_pipeline/transform.py
+++ src/job_data_pipeline/transform.py
@@ -2,9 +2,35 @@
 リクナビNEXTからスクレイピングした求人情報をクレンジング・変換する
 """
 
+import re
+
 import pandas as pd
 
 # ================================
-# 実装時に使用するデータ
+# 処理実装時に使用するデータ
 # ================================
 df = pd.read_csv("/app/data/rikunabi.csv")
+
+
+def clean_company_name(name: str) -> str:
+    """企業名のクレンジング処理
+    -  全角スペースを半角に変換
+    -  本店・支店・東京・大阪などのノイズを削除
+    -  省略表記の正規化（(株) → 株式会社）
+    """
+    if pd.isna(name):
+        return name
+
+    # 全角スペースを半角に変換
+    name = name.replace("　", " ")
+
+    # ノイズ削除
+    name = re.sub(r"(本店|支店|東京|大阪)", "", name)
+
+    # 省略表記の正規化
+    name = re.sub(r"\(株\)", "株式会社", name)
+    name = re.sub(r"\(有\)", "有限会社", name)
+    name = re.sub(r"\（株\）", "株式会社", name)
+    name = re.sub(r"\（有\）", "有限会社", name)
+
+    return name
